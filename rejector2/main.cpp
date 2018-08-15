6581c0a2a5d6af5f785dbeca5e13b5b8dfe593f2
mjobin <oHK-u9f-Tef-u4H>
2018-08-15 14:52:26 -0700

    Initial Commit

---
+++ rejector2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rejector2
+//
+//  Created by matt on 8/15/18.
+//  Copyright © 2018 matt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
